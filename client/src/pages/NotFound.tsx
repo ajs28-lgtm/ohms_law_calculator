65a38696d3c09ab9f40aae61e2dac227f06e69a9
Anjali Sivasothy <anjalisiva@Mac.lan>
2026-05-06 16:34:34 -0500

    Fix GitHub Pages routing

--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -7,7 +7,7 @@ export default function NotFound() {
   const [, setLocation] = useLocation();
 
   const handleGoHome = () => {
-    setLocation("/");
+    setLocation("/ohms_law_calculator/");
   };
 
   return (
